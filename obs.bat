824f89a41832602dece4764d5c7e2e1b4f950ce0
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-09-21 09:52:31 -0700

    Improved OBS Studio launch script

--- obs.bat
+++ obs.bat
@@ -19,18 +19,42 @@ exit /b 0
     !command!
 exit /b
 
+:StartOBS
+    echo [obs-studio] Closing open instances of OBS Studio...
+    taskkill /im "obs-browser-page.exe" /f > nul 2>&1
+    taskkill /im "D:\Projects\obs-studio\build64\install\bin\64bit\obs64.exe" /f > nul 2>&1
+    taskkill /im "obs64.exe" /f > nul 2>&1
+    taskkill /im "OBS Studio" /f > nul 2>&1
+
+    call :TryRunOBS "%~dp0build64\install\bin\64bit"
+    if "%ERRORLEVEL%"=="0" goto:$StartEnd
+
+    call :TryRunOBS "%~dp0build64\rundir\RelWithDebInfo\bin\64bit"
+    if "%ERRORLEVEL%"=="0" goto:$StartEnd
+
+    call :TryRunOBS "C:\Program Files\obs-studio\bin\64bit"
+    if "%ERRORLEVEL%"=="0" goto:$StartEnd
+
+    exit /b 99
+    :$StartEnd
+exit /b 0
+
 :$Main
 
-taskkill /im "obs-browser-page.exe" /f > nul 2>&1
-taskkill /im "D:\Projects\obs-studio\build64\install\bin\64bit\obs64.exe" /f > nul 2>&1
-taskkill /im "obs64.exe" /f > nul 2>&1
-taskkill /im "OBS Studio" /f > nul 2>&1
+call :StartOBS
+if "%ERRORLEVEL%"=="0" (
+    goto:$End
+)
+:: ) else (
+    echo [obs-studio] WARNING: Failed to find OBS Studio executable. Attempting to build from source...
+    if exist "%~dp0build.bat" call "%~dp0build.bat"
+    call :StartOBS
 
-call :TryRunOBS "%~dp0build64\install\bin\64bit"
-if "%ERRORLEVEL%"=="0" goto:eof
+    if errorlevel 1 (
+        echo [obs-studio] ERROR: Failed to find OBS Studio executable even after building source.
+        exit /b 98
+    )
+:: )
 
-call :TryRunOBS "%~dp0build64\rundir\RelWithDebInfo\bin\64bit"
-if "%ERRORLEVEL%"=="0" goto:eof
-
-call :TryRunOBS "C:\Program Files\obs-studio\bin\64bit"
-if "%ERRORLEVEL%"=="0" goto:eof
+:$End
+echo [obs-studio] Launch complete.
